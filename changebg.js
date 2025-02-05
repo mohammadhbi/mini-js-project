4b60f851a4690ad42b278e0d5473fabfbf99c1c7
mohammad <mohammad19hobbi@gmail.com>
2025-02-05 16:24:59 +0330

    random bgcolor completed

---
+++ changebg.js
@@ -0,0 +1,11 @@
+ function getRandomColor(){
+ const letters ='123456789ABCDEF';
+ let color ='#';
+ for (let i=0 ; i<6; i++){
+    color += letters[Math.floor(Math.random() * 16)];
+ }
+return color;
+ }
+ function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomColor();
+}
